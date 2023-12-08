81e5ea067b0267d3e643fada611fdd24b84a6890
Ruslan Agliullin <124513940+rmibogi@users.noreply.github.com>
2023-12-08 06:31:37 +0400

    v 1.4

--- main.py
+++ main.py
@@ -14,19 +14,27 @@ class JpegToDocConverter:
         self.create_widgets()
 
     def create_widgets(self):
-        self.label = tk.Label(self.root, text="Выберите изображение:")
+        font = ('Arial', 12)
+
+        self.label = tk.Label(self.root, text="Выберите изображение:", font=font, wraplength=600)
         self.label.pack(pady=10)
 
-        self.browse_button = tk.Button(self.root, text="Выбрать", command=self.browse_image)
+        self.browse_button = tk.Button(self.root, text="Выбрать", command=self.browse_image, font=font)
         self.browse_button.pack(pady=10)
 
-        self.convert_button = tk.Button(self.root, text="Преобразовать", command=self.convert_image_to_doc)
+        self.convert_button = tk.Button(self.root, text="Преобразовать", command=self.convert_image_to_doc, font=font)
         self.convert_button.pack(pady=10)
+        self.convert_button.config(state=tk.DISABLED)
 
     def browse_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])
-        self.image_path = file_path
-        self.label.config(text=f"Выбрано изображение: {file_path}")
+        if file_path:
+            self.image_path = file_path
+            self.label.config(text=f"Выбрано изображение: {file_path}")
+            self.convert_button.config(state=tk.NORMAL)
+        else:
+            self.label.config(text="Файл не выбран")
+            self.convert_button.config(state=tk.DISABLED)
 
     def convert_image_to_doc(self):
         if hasattr(self, 'image_path'):
@@ -44,6 +52,7 @@ class JpegToDocConverter:
 
 
 root = tk.Tk()
-root.geometry('700x200+200+100')
+root.geometry('700x250+200+100')
+root.resizable(False, False)
 app = JpegToDocConverter(root)
 root.mainloop()
