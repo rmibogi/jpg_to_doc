f8846fd67eb1e7eef8abd1ed1b34d57ba14ed53e
Ruslan Agliullin <124513940+rmibogi@users.noreply.github.com>
2023-12-12 19:31:30 +0400

    v 1.8

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# pyinstaller --onefile --add-data "tesseract-ocr;." main.py
-
 import sys
 import os
 import tkinter as tk
@@ -7,6 +5,7 @@ from tkinter import filedialog
 from PIL import Image
 import pytesseract
 from docx import Document
+import fitz
 
 current_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
 tesseract_path = os.path.join(current_dir, 'tesseract-ocr/tesseract.exe')
@@ -19,36 +18,48 @@ class JpegToDocConverter:
         self.create_widgets()
 
     def create_widgets(self):
-        font = ('Arial', 12)
+        font = ('Arial', 14)
 
-        self.label = tk.Label(self.root, text="Выберите изображение", font=font, wraplength=600)
+        self.label = tk.Label(self.root, text="Выберите изображение или PDF", font=font, wraplength=600)
         self.label.pack(pady=10)
 
-        self.browse_button = tk.Button(self.root, text="Выбрать", command=self.browse_image, font=font)
+        self.browse_button = tk.Button(self.root, text="Выбрать", command=self.browse_file, font=font)
         self.browse_button.pack(pady=10)
 
-        self.convert_button = tk.Button(self.root, text="Преобразовать", command=self.convert_image_to_doc, font=font)
+        self.convert_button = tk.Button(self.root, text="Преобразовать", command=self.convert_to_doc, font=font)
         self.convert_button.pack(pady=10)
         self.convert_button.config(state=tk.DISABLED)
 
         self.file_saved = False
 
-    def browse_image(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])
+    def browse_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Image/PDF files", "*.jpg;*.jpeg;*.png;*.pdf")])
         if file_path:
-            self.image_path = file_path
-            self.label.config(text=f"Выбрано изображение: {file_path}")
+            self.file_path = file_path
+            self.label.config(text=f"Выбрано: {file_path}")
             self.convert_button.config(state=tk.NORMAL)
             self.file_saved = False
         else:
             self.label.config(text="Файл не выбран")
             self.convert_button.config(state=tk.DISABLED)
 
-    def convert_image_to_doc(self):
-        if hasattr(self, 'image_path'):
-            image = Image.open(self.image_path)
+    def convert_to_doc(self):
+        if hasattr(self, 'file_path'):
+            file_extension = os.path.splitext(self.file_path)[-1].lower()
 
-            text = pytesseract.image_to_string(image, lang='rus+eng')
+            if file_extension == '.pdf':
+                images = self.convert_pdf_to_images(self.file_path)
+                text = ""
+
+                for i, image in enumerate(images):
+                    text += pytesseract.image_to_string(image, lang='rus+eng')
+
+                    if i < len(images) - 1:
+                        text += '\n'
+
+            else:
+                image = Image.open(self.file_path)
+                text = pytesseract.image_to_string(image, lang='rus+eng')
 
             doc = Document()
             doc.add_paragraph(text)
@@ -58,6 +69,17 @@ class JpegToDocConverter:
                 doc.save(save_path)
                 self.label.config(text=f"Результат сохранен в {save_path}")
                 self.file_saved = True
+
+    def convert_pdf_to_images(self, pdf_path):
+        images = []
+        doc = fitz.open(pdf_path)
+        for page_num in range(doc.page_count):
+            page = doc[page_num]
+            image = page.get_pixmap()
+            image = Image.frombytes("RGB", [image.width, image.height], image.samples)
+            images.append(image)
+        doc.close()
+        return images
 
 
 root = tk.Tk()
