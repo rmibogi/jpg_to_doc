7353d6a55ee5cd491f198548c2cc3116d92b8cff
Ruslan Agliullin <124513940+rmibogi@users.noreply.github.com>
2023-12-25 02:07:34 +0400

    v 2.3

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# pyinstaller --onefile --add-data "tesseract-ocr;." main.py
-
 import sys
 import os
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
 from docx import Document
-import fitz
 
 current_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
 tesseract_path = os.path.join(current_dir, 'tesseract-ocr/tesseract.exe')
@@ -22,7 +19,7 @@ class JpegToDocConverter:
     def create_widgets(self):
         font = ('Arial', 14)
 
-        self.label = tk.Label(self.root, text="Выберите изображение или PDF", font=font, wraplength=600)
+        self.label = tk.Label(self.root, text="Выберите изображение", font=font, wraplength=600)
         self.label.pack(pady=10)
 
         self.browse_button = tk.Button(self.root, text="Выбрать", command=self.browse_file, font=font)
@@ -35,7 +32,7 @@ class JpegToDocConverter:
         self.file_saved = False
 
     def browse_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image/PDF files", "*.jpg;*.jpeg;*.png;*.pdf")])
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])
         if file_path:
             self.file_path = file_path
             self.label.config(text=f"Выбрано: {file_path}")
@@ -47,21 +44,9 @@ class JpegToDocConverter:
 
     def convert_to_doc(self):
         if hasattr(self, 'file_path'):
-            file_extension = os.path.splitext(self.file_path)[-1].lower()
-
-            if file_extension == '.pdf':
-                images = self.convert_pdf_to_images(self.file_path)
-                text = ""
-
-                for i, image in enumerate(images):
-                    text += pytesseract.image_to_string(image, lang='rus+eng')
-
-                    if i < len(images) - 1:
-                        text += '\n'
-
-            else:
-                image = Image.open(self.file_path)
-                text = pytesseract.image_to_string(image, lang='rus+eng')
+            image = Image.open(self.file_path)
+            preprocessed_image = self.preprocess_image(image)
+            text = pytesseract.image_to_string(preprocessed_image, lang='rus+eng')
 
             doc = Document()
             doc.add_paragraph(text)
@@ -72,18 +57,12 @@ class JpegToDocConverter:
                 self.label.config(text=f"Результат сохранен в {save_path}")
                 self.file_saved = True
 
-
-    def convert_pdf_to_images(self, pdf_path):
-        images = []
-        doc = fitz.open(pdf_path)
-        for page_num in range(doc.page_count):
-            page = doc[page_num]
-            image = page.get_pixmap()
-            image = Image.frombytes("RGB", [image.width, image.height], image.samples)
-            images.append(image)
-        doc.close()
-        return images
-
+    def preprocess_image(self, image):
+        image = image.convert("L")
+        enhancer = ImageEnhance.Contrast(image)
+        image = enhancer.enhance(2.0)
+        image = image.filter(ImageFilter.GaussianBlur(radius=0.3))
+        return image
 
 root = tk.Tk()
 root.geometry('700x250+200+100')
