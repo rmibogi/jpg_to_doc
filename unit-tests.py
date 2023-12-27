198f029b9830a04a875b538de0f8a47f6e7f1784
Ruslan Agliullin <124513940+rmibogi@users.noreply.github.com>
2023-12-27 06:50:53 +0400

    Revert "v 2.7"
    This reverts commit 58aa177bfc8bd2a305344f44cbc482cb54832e1a.

--- unit-tests.py
+++ unit-tests.py
@@ -1,61 +1,62 @@
 import unittest
-from unittest.mock import patch
-from io import StringIO
+from unittest.mock import patch, Mock
+from tkinter import Tk
 from main import JpegToDocConverter
-import tkinter as tk
 
 class TestJpegToDocConverter(unittest.TestCase):
     def setUp(self):
-        self.root = tk.Tk()
-        self.app = JpegToDocConverter(self.root)
+        self.root = Tk()
+        self.converter = JpegToDocConverter(self.root)
 
     def tearDown(self):
         self.root.destroy()
 
-    @patch('tkinter.filedialog.askopenfilename', return_value='path/to/test_image.jpg')
-    def test_browse_file(self, mock_askopenfilename):
-        self.app.browse_file()
-        self.assertEqual(self.app.file_path, 'path/to/test_image.jpg')
-        self.assertEqual(self.app.label.cget('text'), 'Выбрано: path/to/test_image.jpg')
-        self.assertEqual(self.app.convert_button.cget('state'), tk.NORMAL)
+    @patch("tkinter.filedialog.askopenfilename")
+    def test_browse_file_valid_path(self, mock_askopenfilename):
+        mock_askopenfilename.return_value = "path/to/image.jpg"
+        self.converter.browse_file()
+        self.assertEqual(self.converter.file_path, "path/to/image.jpg")
+        self.assertEqual(self.converter.label.cget("text"), "Выбрано: path/to/image.jpg")
+        self.assertEqual(self.converter.convert_button["state"], "normal")
+        self.assertFalse(self.converter.file_saved)
 
-    @patch('tkinter.filedialog.askopenfilename', return_value='')
-    def test_browse_file_no_file_selected(self, mock_askopenfilename):
-        self.app.browse_file()
-        self.assertFalse(hasattr(self.app, 'file_path'))
-        self.assertEqual(self.app.label.cget('text'), 'Файл не выбран')
-        self.assertEqual(self.app.convert_button.cget('state'), tk.DISABLED)
+    @patch("tkinter.filedialog.askopenfilename")
+    def test_browse_file_invalid_path(self, mock_askopenfilename):
+        mock_askopenfilename.return_value = ""
+        self.converter.browse_file()
+        self.assertNotIn("file_path", dir(self.converter))
+        self.assertEqual(self.converter.label.cget("text"), "Файл не выбран")
+        self.assertEqual(self.converter.convert_button["state"], "disabled")
+        self.assertFalse(self.converter.file_saved)
 
-    @patch('tkinter.filedialog.asksaveasfilename', return_value='path/to/save_result.doc')
-    def test_convert_to_doc(self, mock_asksaveasfilename):
-        with patch('builtins.open', create=True) as mock_open:
-            self.app.file_path = 'path/to/test_image.jpg'
-            self.app.convert_to_doc()
+    @patch("docx.opc.pkgwriter.PhysPkgWriter.write")
+    @patch("tkinter.filedialog.asksaveasfilename")
+    @patch("PIL.Image.open")
+    @patch("pytesseract.image_to_string")
+    def test_convert_to_doc(self, mock_image_to_string, mock_image_open, mock_asksaveasfilename,
+                            mock_physpkgwriter_write):
+        mock_asksaveasfilename.return_value = "path/to/save.doc"
+        mock_image_open.return_value = Mock()
+        mock_image_to_string.return_value = "Text from image"
 
-            mock_open.assert_called_once_with('path/to/save_result.doc', 'w', encoding='utf-8')
-            self.assertTrue(self.app.file_saved)
-            self.assertEqual(self.app.label.cget('text'), 'Результат сохранен в path/to/save_result.doc')
+        self.converter.browse_file()
 
-    @patch('tkinter.filedialog.asksaveasfilename', return_value='')
-    def test_convert_to_doc_no_save_path(self, mock_asksaveasfilename):
-        with patch('builtins.open', create=True) as mock_open:
-            self.app.file_path = 'path/to/test_image.jpg'
-            self.app.convert_to_doc()
+        self.converter.convert_to_doc()
 
-            mock_open.assert_not_called()
-            self.assertFalse(self.app.file_saved)
-            self.assertEqual(self.app.label.cget('text'), 'Файл не выбран')
+        mock_image_open.assert_called_with(self.converter.file_path)
+        mock_image_to_string.assert_called_with(mock_image_open.return_value, lang='rus+eng')
+        mock_physpkgwriter_write.assert_called_with("path/to/save.doc", self.converter.doc_part.rels,
+                                                    self.converter.doc_part.parts)
 
-    def test_preprocess_image(self):
-        image_path = 'path/to/test_image.jpg'
-        with patch('PIL.Image.open') as mock_open:
-            with patch.object(Image.Image, 'convert') as mock_convert:
-                with patch.object(Image.Image, 'filter') as mock_filter:
-                    self.app.preprocess_image(image_path)
+        self.assertTrue(self.converter.file_saved)
+        self.assertIn("Результат сохранен в", self.converter.label.cget("text"))
 
-                    mock_open.assert_called_once_with(image_path)
-                    mock_convert.assert_called_once_with('L')
-                    mock_filter.assert_called_once_with(ImageFilter.GaussianBlur(radius=0.3))
+    @patch("tkinter.filedialog.asksaveasfilename")
+    def test_convert_to_doc_cancel_save(self, mock_asksaveasfilename):
+        mock_asksaveasfilename.return_value = ""
+        self.converter.convert_to_doc()
+        self.assertEqual(self.converter.label.cget("text"), "Выберите изображение")
+        self.assertFalse(self.converter.file_saved)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
